bbacb0fa7906202d76c4e31125734a2352e86446
Maksym <mysakmakslm26@gmail.com>
2025-06-05 13:38:55 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,41 +1,17 @@
-// Import the functions you need from the SDKs you need 
-// import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-/*const firebaseConfig = {
-  apiKey: "AIzaSyBRX6G_3mYajBcCQORjYKDctSFbx2Uk2aM",
-  authDomain: "auth-manager-ebaea.firebaseapp.com",
-  projectId: "auth-manager-ebaea",
-  storageBucket: "auth-manager-ebaea.firebasestorage.app",
-  messagingSenderId: "476071800063",
-  appId: "1:476071800063:web:b1556a0989c1596b1370ca",
-  measurementId: "G-KEF925NHGE"
-};*/
-
-// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // Додаємо модуль для аутентифікації
-import { getFirestore } from "firebase/firestore"; // Додаємо модуль для Firestore
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRX6G_3mYajBcCQORjYKDctSFbx2Uk2aM",
-  authDomain: "auth-manager-ebaea.firebaseapp.com",
-  projectId: "auth-manager-ebaea",
-  storageBucket: "auth-manager-ebaea.firebasestorage.app",
-  messagingSenderId: "476071800063",
-  appId: "1:476071800063:web:b1556a0989c1596b1370ca",
-  measurementId: "G-KEF925NHGE"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // Експортуємо auth для аутентифікації
-export const db = getFirestore(app); // Експортуємо db для Firestore
+export const auth = getAuth(app);
+export const db = getFirestore(app);
